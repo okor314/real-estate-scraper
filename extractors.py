2b4caff8404eb49f08c99545bb11df55c06448dd
okor314 <korol.oleg10@gmail.com>
2025-10-05 17:00:35 +0300

    Create extractor classes
    Classes contains instructions how to get data from different json objects

---
+++ extractors.py
@@ -0,0 +1,51 @@
+class Extractor:
+    def __init__(self):
+        self.pathToData: list
+
+    def extract(self, response_json):
+        dataContainer = response_json
+        for key in self.pathToData:
+            dataContainer = dataContainer.get(key)
+        
+        return dataContainer
+    
+class CatalogExtractor(Extractor):
+    def __init__(self):
+        self.pathToData = ['cat1', 'searchResults', 'listResults']
+
+    def extract(self, response_json):
+        dataContainer = response_json
+        for key in self.pathToData:
+            dataContainer = dataContainer.get(key)
+
+        result = []
+
+        for item in dataContainer:
+            result.append(
+                {
+                    'status': item.get('statusType'),
+                    'zpid': item.get('zpid'),
+                    'price': item.get('unformattedPrice'),
+                    'beds': item.get('beds'),
+                    'baths': item.get('baths'),
+                    'area': item.get('area'),
+                    'street': item.get('addressStreet'),
+                    'city': item.get('addressCity'),
+                    'state': item.get('addressState'),
+                    'zipcode': item.get('addressZipcode'),
+                    'latitude': item.get('latLong').get('latitude'),
+                    'longitude': item.get('latLong').get('longitude'),
+                    'url': item.get('detailUrl')
+                }
+            )
+        return result
+    
+class CatalogDomExtractor(CatalogExtractor):
+    def __init__(self):
+        super().__init__()
+        self.pathToData = ['props', 'pageProps', 'searchPageState', 'cat1', 'searchResults', 'listResults']
+    
+if __name__ == "__main__":
+    d = {'cat1': {'searchResults': {'listResults': 'pupupu'}}}
+    print(CatalogExtractor().extract(d))
+    
